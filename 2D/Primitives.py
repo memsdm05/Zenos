8ff800b4dd4799b4543d72fada974427c2f5580a
22browner <22browner@da.org>
2020-02-13 20:41:33 -0500

    Initial commit

---
+++ 2D/Primitives.py
@@ -0,0 +1,27 @@
+class _Primative():
+    pass
+
+class Polygon(_Primative):
+    pass
+
+class Quad(Polygon):
+    pass
+
+class Rectangle(Quad):
+    pass
+
+class Square(Rectangle):
+    pass
+
+class Triangle(Polygon):
+    pass
+
+class Line(_Primative):
+    pass
+
+class Point(_Primative):
+    pass
+
+class Circle():
+    pass
+
