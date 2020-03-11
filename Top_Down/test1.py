65eff4543468cd7807f2b91257003856e6fbe2d3
TateStaples <56731352+tatestaples@users.noreply.github.com>
2020-03-11 14:58:28 -0400

    basic example of the new system

---
+++ Top_Down/test1.py
@@ -0,0 +1,20 @@
+from Top_Level import Window
+import _2D
+import Resources
+
+
+class TestWindow(Window):
+    c = _2D.Square((100, 100), 10)
+
+    def periodic(self, dt):
+        self.clear()
+        self.c.render_outline()
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        self.c.moveTo((x, y))
+
+
+if __name__ == '__main__':
+    x = TestWindow()
+    x.set_background(Resources.RED)
+    x.start()
